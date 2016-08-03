bedec4efe89aecd0b0d8e2b10fdf036dfa974aea
blues <blues@blues.com>
2016-08-04 01:44:27 +0800

    Set adapter

--- app/src/main/java/com/blues/popular_movie_stage1/MovieFragement.java
+++ app/src/main/java/com/blues/popular_movie_stage1/MovieFragement.java
@@ -1,10 +1,17 @@
 package com.blues.popular_movie_stage1;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 
 public class MovieFragement extends Fragment {
@@ -25,7 +32,63 @@ public class MovieFragement extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_fragment_main,container, false);
+        GridView gridView = (GridView) rootView.findViewById(R.id.gridView);
+        gridView.setAdapter(new ImageAdapter(getActivity()));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //TODO: click Trigger
+            }
+        });
+
+        return rootView;
+    }
+
+    public class ImageAdapter extends BaseAdapter{
+        private String[] imageUrls = {
+                // TODO: imageUrls
+        };
+
+        private Context mContext;
+
+        public ImageAdapter(Context c){
+            mContext = c;
+        }
+
+        @Override
+        public int getCount() {
+//            return imageUrls.length;
+            return 9;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if (convertView == null){
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new ViewGroup.LayoutParams(85,85));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(8,8,8,8);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            Picasso.with(mContext).load("http://i.imgur.com/DvpvklR.png").into(imageView);
+
+            return imageView;
+        }
     }
 
 }
