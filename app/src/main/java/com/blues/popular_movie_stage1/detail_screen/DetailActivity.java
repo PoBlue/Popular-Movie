6d2b3e1754b9bed354b08c8499eb16d59a6ff325
PoBlue <250299430@qq.com>
2017-09-01 12:14:48 +0800

    add: reviews & trailer list view

--- app/src/main/java/com/blues/popular_movie_stage1/detail_screen/DetailActivity.java
+++ app/src/main/java/com/blues/popular_movie_stage1/detail_screen/DetailActivity.java
@@ -2,24 +2,32 @@ package com.blues.popular_movie_stage1.detail_screen;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.blues.popular_movie_stage1.R;
 import com.blues.popular_movie_stage1.model.Movie;
+import com.blues.popular_movie_stage1.model.Review;
+import com.blues.popular_movie_stage1.model.Trailer;
+import com.blues.popular_movie_stage1.root.App;
 import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements DetailActivityMVP.View {
 
     private final String LOG_TAG = DetailActivity.class.getSimpleName();
     public static final String ARG_MOVIE = "ARG_MOVIE";
@@ -31,22 +39,41 @@ public class DetailActivity extends AppCompatActivity {
     @Bind(R.id.detail_toolbar)
     Toolbar mToolbar;
 
+    @Bind(R.id.movie_reviews)
+    RecyclerView movieReviewsRecycler;
+
+    @Bind(R.id.movie_videos)
+    RecyclerView movieTrailersRecycler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         ButterKnife.bind(this);
+        ((App) getApplication()).getComponent().inject(this);
 
-        setSupportActionBar(mToolbar);
         Intent intent = getIntent();
         mMovie = intent.getParcelableExtra(ARG_MOVIE);
+        presenter.setView(this);
+        presenter.getMovieInfo(mMovie);
 
-        if (intent != null) {
-            String title = mMovie.getTitle();
-            String posterUrl = mMovie.getPosterUrl(this);
-            String overview = mMovie.getOverview();
-            String vote = mMovie.getUserRating();
-            String date = mMovie.getReleaseDate();
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+
+        return super.onCreateView(name, context, attrs);
+    }
+
+    @Override
+    public void showMovieInfo(Movie movie) {
+        if (movie != null) {
+            String title = movie.getTitle();
+            String posterUrl = movie.getPosterUrl(this);
+            String overview = movie.getOverview();
+            String vote = movie.getUserRating();
+            String date = movie.getReleaseDate();
 
             ((TextView) findViewById(R.id.movie_title)).setText(title);
             ((TextView) findViewById(R.id.movie_overview)).setText(overview);
@@ -55,16 +82,35 @@ public class DetailActivity extends AppCompatActivity {
 
             Picasso.with(this)
                     .load(posterUrl)
-                    .into((ImageView) findViewById( R.id.back_drop));
+                    .into((ImageView) findViewById(R.id.back_drop));
 
             Picasso.with(this)
                     .load(posterUrl)
-                    .into((ImageView) findViewById( R.id.movie_poster));
+                    .into((ImageView) findViewById(R.id.movie_poster));
         }
     }
 
     @Override
-    public View onCreateView(String name, Context context, AttributeSet attrs) {
-        return super.onCreateView(name, context, attrs);
+    public void showReviewInfo(List<Review> reviews) {
+
+    }
+
+    @Override
+    public void showTrailerInfo(List<Trailer> trailers) {
+
+    }
+
+    @Override
+    public void openTrailer(Trailer trailer) {
+        Uri webpage = Uri.parse(trailer.getTrailerUrl());
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void showErrorMessage(String msg) {
+        Log.v("test", msg);
     }
 }
